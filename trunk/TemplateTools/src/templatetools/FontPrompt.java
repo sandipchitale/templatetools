c7da829603263fb94c2df30695d8a33e8534afdb
sandipchitale <sandipchitale@0c08a8be-8dc2-11dd-b496-dd7b60ab8c28>
2008-09-30 03:44:34 +0000

    Initial check in.

---
+++ trunk/TemplateTools/src/templatetools/FontPrompt.java
@@ -0,0 +1,22 @@
+package templatetools;
+
+import java.util.List;
+
+import org.eclipse.swt.graphics.FontData;
+import org.eclipse.swt.widgets.FontDialog;
+import org.eclipse.ui.PlatformUI;
+
+public class FontPrompt implements IPrompt {
+
+	@SuppressWarnings("unchecked")
+	public String getValue(String name, List params) {
+		FontDialog fontDialog = new FontDialog(PlatformUI.getWorkbench()
+				.getActiveWorkbenchWindow().getShell());
+		FontData fontData = fontDialog.open();
+		if (fontData != null) {
+			return fontData.toString();
+		}
+		return null;
+	}
+
+}
